b5f9b54b0c6cee5bd8eab52e7fa5bc5b74092150
JordanoBaluz <jordanolb95@hotmail.com>
2022-04-28 20:37:34 -0300

    created the component for web menu

--- src/header/web/index.js
+++ src/header/web/index.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Web() {
-  return <div>Web</div>;
+  return (
+    <div className="web">
+      <div className="web-option">
+        <a href="#project">
+          <i class="fi-rr-edit-alt option-icon"></i>
+          Projeto
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#skills">
+          <i class="fi-rr-laptop option-icon"></i>
+          Habilidades
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#work">
+          <i class="fi-rr-briefcase option-icon"></i>
+          Trabalhos
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#contact">
+          <i class="fi-rr-user option-icon"></i>
+          Contato
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Web;
