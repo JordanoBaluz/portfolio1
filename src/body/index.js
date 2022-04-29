73e7760040fc6bb2a11ff989a9b58f3b1ffc5455
JordanoBaluz <jordanolb95@hotmail.com>
2022-04-28 21:18:22 -0300

    created the components projects, skills, works, contact and imported in body component

--- src/body/index.js
+++ src/body/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Contact from "./contact";
+import Works from "./works";
+import Skills from "./skills";
+import Projects from "./projects";
 
 function Body() {
-  return <div>este é o corpo</div>;
+  return (
+    <div className="body">
+      <section id="project">
+        <Projects />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="work">
+        <Works />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </div>
+  );
 }
 
 export default Body;
